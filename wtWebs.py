31fc1095518a5d403c98854fce7e6e925c3ba0be
ayamgokheng <96785209+ayamgokheng@users.noreply.github.com>
2021-12-28 10:43:58 -0500

    Add files via upload
    wtWebs, all small websites weakness.

---
+++ wtWebs.py
@@ -0,0 +1,33 @@
+import time
+
+print("""
+░██╗░░░░░░░██╗████████╗░██╗░░░░░░░██╗███████╗██████╗░░██████╗
+░██║░░██╗░░██║╚══██╔══╝░██║░░██╗░░██║██╔════╝██╔══██╗██╔════╝
+░╚██╗████╗██╔╝░░░██║░░░░╚██╗████╗██╔╝█████╗░░██████╦╝╚█████╗░
+░░████╔═████║░░░░██║░░░░░████╔═████║░██╔══╝░░██╔══██╗░╚═══██╗
+░░╚██╔╝░╚██╔╝░░░░██║░░░░░╚██╔╝░╚██╔╝░███████╗██████╦╝██████╔╝
+
+       weakness of all small websites in the world.
+""")
+
+while True:
+	i = 0
+	pw = input("Password(hint: sm_l_w__kn__s): ")
+	if pw == "smallweakness":
+		web = input("Enter Website name(e.g www.website.com): ")
+		print("searching website...")
+		time.sleep(3)
+		print("Force accessing website...")
+		time.sleep(5)
+		print("looking for weakness...")
+		time.slee(6)
+		for i in range(1, 101, 1):
+			print("hacking...", str(i) + "%")
+			i += 1
+			time.sleep(0.1)
+		print("Website Hacked!")
+		continue
+	
+	else:
+		print("WRONG PASSWORD!")
+		continue
